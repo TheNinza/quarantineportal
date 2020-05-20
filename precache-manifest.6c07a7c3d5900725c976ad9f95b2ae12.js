c5b7cbee2604c77b6e24da4af3c65cb00fc47355
TheNinza <niks.a3198@gmail.com>
2020-05-20 14:35:22 +0530

    Updates

---
+++ precache-manifest.6c07a7c3d5900725c976ad9f95b2ae12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1fcee9bda1dceb81c89845149ce62c",
+    "url": "/quarantineportal/index.html"
+  },
+  {
+    "revision": "ed3a0acb19a15eaa2ca8",
+    "url": "/quarantineportal/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dd53e1bbcd23d1136fad",
+    "url": "/quarantineportal/static/css/main.ed9638e2.chunk.css"
+  },
+  {
+    "revision": "ed3a0acb19a15eaa2ca8",
+    "url": "/quarantineportal/static/js/2.fa35535d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/quarantineportal/static/js/2.fa35535d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd53e1bbcd23d1136fad",
+    "url": "/quarantineportal/static/js/main.01ac10f0.chunk.js"
+  },
+  {
+    "revision": "9a620be0b7d92ac6056e",
+    "url": "/quarantineportal/static/js/runtime-main.49ea12f4.js"
+  }
+]);
